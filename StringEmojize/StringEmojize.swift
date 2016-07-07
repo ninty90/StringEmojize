0cc56cdb2bc825881b66c1c17e5620b2f01d30d1
little2s <duanyinglun@gmail.com>
2016-07-07 16:05:13 +0800

    update

--- StringEmojize/StringEmojize.swift
+++ StringEmojize/StringEmojize.swift
@@ -10,39 +10,11 @@ import Foundation
 
 extension String {
     
-    public func emojizedString() -> String {
-        return self.emojizedStringWithString(self)
+    public var nty_emojized: String {
+        return nty_emojiMapping(self)
     }
     
-    public func emojizedStringWithString(text: String) -> String {
-        var onceToken: dispatch_once_t = 0
-        var regex = NSRegularExpression()
-        
-        dispatch_once(&onceToken) {
-            regex = NSRegularExpression(pattern: "(:[a-z0-9-+_]+:)", options: .CaseInsensitive, error:nil)!
-        }
-        
-        var resultText = text
-        let matchingRange = NSMakeRange(0, resultText.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-        regex.enumerateMatchesInString(resultText, options: .ReportCompletion, range: matchingRange, usingBlock: {
-            (result: NSTextCheckingResult!, flags: NSMatchingFlags, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
-                if ((result != nil) && (result.resultType == .RegularExpression)) {
-                    let range = result.range
-                    if (range.location != NSNotFound) {
-                        var code = (text as NSString).substringWithRange(range)
-                        var unicode = self.emojiAliases(code)
-                        if !unicode.isEmpty {
-                            resultText = resultText.stringByReplacingOccurrencesOfString(code, withString:unicode, options: nil, range: nil)
-                        }
-                    }
-                }
-        })
-        
-        return resultText
-    }
-    
-    func emojiAliases(key: String) -> String {
-        //var myDict = Dictionary<String, String>()
+    func nty_emojiMapping(key: String) -> String {
         var value: String = ""
         var onceToken: dispatch_once_t = 0
         dispatch_once(&onceToken) {
@@ -51,4 +23,5 @@ extension String {
         
         return value
     }
+    
 }
